1d68d4444056cedcc625f2078ef326db28109c4c
taiwing <yoyo.fofo47@gmail.com>
2018-04-22 18:37:34 +0200

    180422_backup

--- 180405A-tag/src/match.c
+++ 180405A-tag/src/match.c
@@ -1,7 +1,6 @@
 #include "include/tag_query.h"
 
 /*TODO*/
-/*First: test that everything is working as expected*/
 /*Dos: make valid_query verify rel_ops too*/
 /*Tres: Make some functions shorter*/
 /*Quatro: maybe divide this file into multiple files*/
@@ -16,6 +15,9 @@ char *ro_str[] = {
 	"<",
 };
 
+static int check_par(char *query, size_t l, int *varray);
+static int check_charplace(char *query, size_t l, int *varray);
+static int check_rel_ops(char *query, size_t l, int *varray);
 static int weight(char **expr, size_t *n);
 static int get_log_op(char *query, size_t n, char **p, size_t *j);
 static int get_rel_op(char *query, size_t n, char **r);
@@ -27,74 +29,124 @@ static int dt_type_match(query_node *nd, int i, const char *f, void *val[2]);
 /*tests if the query is valid*/
 int valid_query(char *query)
 {
-	int err = 0;
-	int rpar = 0, lpar = 0, nest = 0, l = strlen(query);
-	int *parray = (int *)salloc(l * sizeof(int));
-	for(int i = 0; i < l; i++)
-	{
-		if(query[i] == LPAR)
-			lpar++;
-		nest = lpar - rpar;
-		switch(query[i])
-		{
-			case LPAR:
-			case RPAR: parray[i] = nest;
-					break;
-			case AND:
-			case OR:
-			case XOR: parray[i] = -2;
-					break;
-			case NOT: parray[i] = -1;
-					break;
-			case ANT: parray[i] = 0;
-				if(i < l-1)
-					parray[++i] = 0;
-				else
-				 	err = 1;
-				break;
-			default:  parray[i] = 0;
-					break;
-		}
-		if(query[i] == RPAR)
-			rpar++;
-		if(lpar - rpar < 0) /*if there's more RPAR than LPAR*/
-			err = 1;
-	}
+	size_t l = strlen(query);
+	int (*check)(char *, size_t, int *);
+	int err = 0, *verif_array = (int *)salloc(l * sizeof(int));
 
-	if(lpar != rpar) /*if there's not the same total of LPAR and RPAR*/
-		err = 1;
-
-	for(int i = 0; i < l; i++) /*rules regarding character placement*/
+	for(int i = 0; i < 3 && err == 0; i++)
 	{
-		switch(parray[i])
-		{
-			case -2:
-				if(i == 0 || i == l-1
-					|| (query[i-1] == '(' && parray[i-1] != 0)
-					|| query[i+1] == ')'
-					|| parray[i+1] == -2)
-					err = 1;
-				break;
-			case -1:
-				if(i == l-1 || query[i+1] == ')' || parray[i+1] == -2)
-					err = 1;
-				break;
-			case 0:
-				if((i != 0 && query[i-1] == ')' && parray[i-1] != 0)
-					|| (i != l-1 && query[i+1] == '('))
-					err = 1;
-				break;
-		}
+		check = i == 0 ? &check_par :
+						i == 1 ? &check_charplace :
+						i == 2 ? &check_rel_ops :
+						NULL;
+		err = (*check)(query, l, verif_array);
 	}
-	free(parray);
 
 	if(err)
 	{
 		fprintf(stderr, "%s: incorrect query\n", PROGNAME);
 		exit(EXIT_FAILURE);
 	}
-	else
-		return 1;
+
+	free(verif_array);
+	return 1;
+}
+
+static int check_par(char *query, size_t l, int *varray)
+{
+	int rpar = 0, lpar = 0, nest = 0;
+	for(size_t i = 0; i < l; i++)
+	{
+		lpar += query[i] == LPAR ? 1 : 0;
+		nest = lpar - rpar;
+		switch(query[i])
+		{
+			case LPAR:
+			case RPAR: varray[i] = nest;
+					break;
+			case AND:
+			case OR:
+			case XOR: varray[i] = -2;
+					break;
+			case NOT: varray[i] = -1;
+					break;
+			case ANT: varray[i] = 0;
+				if(i < l-1)
+					varray[++i] = 0;
+				else
+				 	return 1;
+				break;
+			default:  varray[i] = 0;
+					break;
+		}
+		rpar += query[i] == RPAR ? 1 : 0;
+		if(lpar - rpar < 0) return 1;
+	}
+	return (lpar != rpar);
+}
+
+static int check_charplace(char *query, size_t l, int *varray)
+{
+	for(size_t i = 0; i < l; i++) /*rules regarding character placement*/
+	{
+		switch(varray[i])
+		{
+			case -2:
+				if(i == 0 || i == l-1
+					|| (query[i-1] == '(' && varray[i-1] != 0)
+					|| query[i+1] == ')'
+					|| varray[i+1] == -2)
+					return 1;
+				break;
+			case -1:
+				if(i == l-1 || query[i+1] == ')' || varray[i+1] == -2)
+					return 1;
+				break;
+			case 0:
+				if((i != 0 && query[i-1] == ')' && varray[i-1] != 0)
+					|| (i != l-1 && query[i+1] == '('))
+					return 1;
+				break;
+		}
+	}
+	return 0;
+}
+
+static int check_rel_ops(char *query, size_t l, int *varray)
+{
+	for(int i = 1; i < 7; i++)
+	{
+		char *p = query, *r = NULL;
+		do
+		{
+			r = strstr(p, ro_str[i]);
+			p = r ? r+strlen(ro_str[i]) : p;
+			if(r == query || p == &(query[l-1]))
+				return 1;
+			else if(r > query && *(r-1) != '\\') /*if it is unescaped*/
+			{
+				int rp = r-query; /*rel_op place*/
+				if((varray[rp-1] < 0 || query[rp-1] == LPAR)
+				  || (varray[p-query] < 0 || p[0] == RPAR))
+					return 1;
+				else
+				{
+					size_t llr[2] = {0, 0};
+					char *left = r, *right = p;
+					while(varray[left-query-1] == 0 && left > query)
+						left--;
+					llr[0] = r-left;
+					while(varray[right-query+1] == 0 && right < query+l)
+						right++;
+					llr[1] = right-p;
+					if(is_quoted(left, llr[0]) && is_quoted(p, llr[1]))
+						return 1;
+				}
+			}
+		}
+		while(r);
+	}
+	return 0;
 }
 
 /*if it is, this one builds the structure for it*/
