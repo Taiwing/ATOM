57f509bb34c38b1affd920d8b36442f801e71602
taiwing <yoyo.fofo47@gmail.com>
2018-05-18 09:35:04 +0200

    180518_backup

--- 180512A-tagex/src/include/tagex.h
+++ 180512A-tagex/src/include/tagex.h
@@ -3,6 +3,13 @@
 
 #include "main.h"
 
+/*sorting modes*/
+#define DEFAULT 		0x03	/*SEPTF + ALPHANUM*/
+#define SEPTF				0x01	/*separate tags and files*/
+#define ALPHANUM		0x02
+#define SIZE				0x04
+#define TAG_VALUE		0x08
+
 enum types {TAG = 'T', REGFILE = 'F'};
 
 /*elements displayed on the tagex screen*/
@@ -10,8 +17,8 @@ typedef struct tag_elem
 {
 	char *name;		/*displayed name*/
 	char *file;		/*file location*/
-	size_t size;	/*size of the file and number of elemets in a tag_folder*/
-	char type;
+	size_t size;	/*size of the file and number of elements in a tag_folder*/
+	char type;		/*TAG or REGFILE*/
 } tag_elem;
 
 void ncint_tagex(char *root_dir);
--- 180512A-tagex/src/tagex.c
+++ 180512A-tagex/src/tagex.c
@@ -4,6 +4,9 @@ static void get_raw_vals(char *root_dir, char **qbar, int qbc,
 												lout **raw_tags, qout **raw_files);
 static qout *get_uniqs(lout *raw_tags, char *query, char *root_dir);
 static char *build_query(char **qbar, int qbc);
+static void update_elem_list(tag_elem ***elem_list, size_t *elsize, int *elc,
+														 lout **raw_tags, qout **raw_files);
+void sort_elem_list(tag_elem **elem_list, int elc, uint8_t sortmode);
 
 void ncint_tagex(char *root_dir)
 {
@@ -11,12 +14,16 @@ void ncint_tagex(char *root_dir)
 	tag_elem **elem_list = NULL;				/*tags and files*/
 	int qbc = 0, elc = 0, noexit = 1;
 	size_t qbsize = 0, elsize = 0;			/*size of query_bar and elem_list*/
+	uint8_t sortmode = DEFAULT;
 	qout *raw_files = NULL;							/*output of a query call*/
 	lout *raw_tags = NULL;							/*output of a list call*/
 
 	while(noexit)
 	{
 		get_raw_vals(root_dir, query_bar, qbc, &raw_tags, &raw_files);
+		update_elem_list(&elem_list, &elsize, &elc, &raw_tags, &raw_files);
+		sort_elem_list(elem_list, elc, sortmode);
+
 	}
 }
 
@@ -41,14 +48,30 @@ static qout *get_uniqs(lout *raw_tags, char *query, char *root_dir)
 {
 	qout *out = (qout *)salloc(sizeof(qout)), *tmp = NULL;
 	size_t flsize = 0;
-
 	out->file_list = NULL;
 	out->fc = 0;
+
 	for(int i = 1; i < raw_tags->tc; i++)
-		if(raw_tags->taglist[i]->c == 1) /*TODO: finish this shit*/
+		if(raw_tags->taglist[i]->c == 1)
 		{
-			tmp = tag_query(query ? query : raw_tags->taglist[i]->name,
+			char *q = NULL;
+			if(query) /*add the tag name to the query*/
+			{
+				q = (char *)salloc(strlen(query)+4);
+				strcpy(q, query);
+				strcat(q, "&(");
+				strcat(q, raw_tags->taglist[i]->name);
+				strcat(q, ")");
+			}
+			tmp = tag_query(query ? q : raw_tags->taglist[i]->name,
 											OPT_RECURSIVE | OPT_ALL, &root_dir, 1);
+
+			walloc((void ***)&(out->file_list), &flsize, &(out->fc));
+			out->file_list[out->fc-1] = tmp->file_list[0];
+			tmp->file_list[0] = NULL; /*to avoid freeing*/
+
+			if(q) free(q);
+			free(tmp);
 		}
 
 	if(out->fc == 0)
@@ -84,4 +107,56 @@ static char *build_query(char **qbar, int qbc)
 			}
 		return ret;
 	}
+}
+
+static void update_elem_list(tag_elem ***elem_list, size_t *elsize, int *elc,
+														 lout **raw_tags, qout **raw_files)
+{
+	if(*raw_tags)
+	{
+		for(int i = 0; i < (*raw_tags)->tc; i++)
+		{
+			tag_elem *new = (tag_elem *)salloc(sizeof(tag_elem));
+			new->name = (*raw_tags)->taglist[i]->name;
+			(*raw_tags)->taglist[i]->name = NULL; /*to avoid freeing*/
+			new->size = (*raw_tags)->taglist[i]->c;
+			new->type = TAG;
+			walloc((void ***)elem_list, elsize, elc);
+			(*elem_list)[(*elc)-1] = new;
+		}
+		free(*raw_tags);
+		*raw_tags = NULL;
+	}
+
+	if(*raw_files)
+	{
+		for(int i = 0; i < (*raw_files)->fc; i++)
+		{
+			int j;
+			struct stat file_stat;
+			tag_elem *new = (tag_elem *)salloc(sizeof(tag_elem));
+			new->file = (*raw_files)->file_list[i];
+			(*raw_files)->file_list[i] = NULL; /*to avoid freeing*/
+			for(j = strlen(new->file)-1; j >= 0 && new->file[j] != '/'; j--);
+			new->name = new->file[j] == '/' ? &(new->file[j+1]) : new->file;
+			stat(new->file, &file_stat);
+			new->size = (size_t)file_stat.st_size;
+			new->type = REGFILE;
+			walloc((void ***)elem_list, elsize, elc);
+			(*elem_list)[(*elc)-1] = new;
+		}
+		free(*raw_tags);
+		*raw_tags = NULL;
+	}
+}
+
+/*TODO: implement tagvalue_sort*/
+void sort_elem_list(tag_elem **elem_list, int elc, uint8_t sortmode)
+{
+	if(sortmode & SEPTF)
+		septf_sort(elem_list, elc);
+	if(sortmode & ALPHANUM || sortmode & TAG_VALUE) /*temp*/
+		alphanum_sort(elem_list, elc, sortmode & SEPTF);
+	if(sortmode & SIZE)
+		size_sort(elem_list, elc, sortmode & SEPTF);
 }
