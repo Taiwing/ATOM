ca7250fcfc2efc6b77b144e97cb7f49e0a9347b8
taiwing <yoyo.fofo47@gmail.com>
2018-05-24 22:41:13 +0200

    180524_backup

--- 000_Tests/180511A-ncurses/nctest/nc.c
+++ 000_Tests/180511A-ncurses/nctest/nc.c
@@ -5,6 +5,7 @@
 /*TODO*/
 /*remake the printing functions so they don't need global variables*/
 /*print "tag_folders" in cyan*/
+/*reorganise all this shit, try to reduce the number of variables in tagst*/
 
 char *qbar[] = {
 	"tag_adress",
@@ -88,7 +89,7 @@ char *func_panel_query[] = {
 };
 
 /*global variable for the handle_winch function*/
-WINDOW *win[4]; /*qbar, elem_list, functions, function_panel*/
+WINDOW *win[5]; /*qbar, elem_list, functions, function_panel*/
 /*and maybe add a command line*/
 
 typedef struct tag_status
@@ -100,6 +101,7 @@ typedef struct tag_status
 	int old_hlight;						 /*save hlight when a function panel is displayed*/
 	int choice;																		 /*element chosen by the user*/
 	int c;																												 /*user input*/
+	int panel_ev;										 /*signals when a penel is opened or closed*/
 
 	/*qbar*/
 	int qbc;																								/*number of queries*/
@@ -111,21 +113,26 @@ typedef struct tag_status
 
 	/*function panels*/
 	int func;																				/*currently displayed panel*/
+
+	/*subfunction panels*/
+	int sub_func;																 /*currently displayed subpanel*/
 }	tagst;
 
-void generate_win(WINDOW *wind[4]);
-void function_panel(WINDOW *wind[4], tagst *ts);
+void generate_win(WINDOW *wind[5]);
+void function_panel(WINDOW *wind[5], tagst *ts);
+void sub_function_panel(WINDOW *wind[5], tagst *ts);
 void handle_winch(int sig);
 void mvhlight(tagst *ts);
 void mvhlight_qbar(tagst *ts);
 void mvhlight_elem_list(tagst *ts);
 void mvhlight_funcpanel(tagst *ts);
 void function_input(tagst *ts, int *noexit);
-void print_tag_screen(WINDOW *wind[4], tagst *ts);
+void print_tag_screen(WINDOW *wind[5], tagst *ts);
 void print_qbar(WINDOW *qbar_win, tagst *ts);
 void print_elem_list(WINDOW *elist_win, tagst *ts);
 void print_funcbar(WINDOW *funcbar_win, tagst *ts);
 void print_funcpanel(WINDOW *funcpanel, tagst *ts);
+void print_subfuncpanel(WINDOW *subfunc, tagst *ts);
 
 int main(void)
 {
@@ -140,10 +147,12 @@ int main(void)
 	ts.old_hlight = -1;															 /*no old_hlight bu default*/
 	ts.choice = -1;																							/*no choice yet*/
 	ts.curq = 2;					/*2 for the example, but no query yet (-1) by default*/
+	ts.panel_ev = 0;																						 /*0 by default*/
 	ts.qbc = 4;														/*4 for the example, but 0 by default*/
 	ts.elc = 4;														/*4 for the example, but 0 by default*/
 	ts.attrc = 3;					/*3 for the example, but 2 by default (name and size)*/
 	ts.func = -1;																/*no panel activated by default*/
+	ts.sub_func = -1;												 /*no subpanel activated by default*/
 
 	/*sigaction initalization*/
 	memset(&sa, 0, sizeof(struct sigaction));
@@ -169,6 +178,7 @@ int main(void)
 		function_input(&ts, &noexit);
 		function_panel(win, &ts);
 		mvhlight(&ts);
+		sub_function_panel(win, &ts);
 		print_tag_screen(win, &ts);
 		if(ts.choice != -1)
 			noexit = 0;
@@ -187,18 +197,19 @@ int main(void)
 	return 0;
 }
 
-void generate_win(WINDOW *wind[4])
+void generate_win(WINDOW *wind[5])
 {
 	wind[0] = newwin(1, COLS, 0, 0);																		 /*qbar*/
 	wind[1] = newwin(LINES-2, COLS, 1, 0);													/*elem_list*/
 	wind[2] = newwin(1, COLS, LINES-1, 0);													/*functions*/
 	wind[3] = NULL;																						 /*function panel*/
+	wind[4] = NULL;																					/*sub-funcion panel*/
 
 	for(int i = 0; i < 3; i++)	/*we get F1, F2, etc... and the arrows*/
 		keypad(wind[i], TRUE);
 }
 
-void function_panel(WINDOW *wind[4], tagst *ts)
+void function_panel(WINDOW *wind[5], tagst *ts)
 {
 	if(wind[3] && ts->func == -1)
 	{
@@ -207,6 +218,7 @@ void function_panel(WINDOW *wind[4], tagst *ts)
 		ts->curwin = ts->old_curwin;
 		ts->old_curwin = 3;
 		ts->hlight = ts->old_hlight;
+		ts->panel_ev = 1;
 	}
 	else if(wind[3] == NULL && ts->func != -1)
 	{
@@ -237,12 +249,41 @@ void function_panel(WINDOW *wind[4], tagst *ts)
 	}
 }
 
+void sub_function_panel(WINDOW *wind[5], tagst *ts)
+{
+	if(wind[4] && ts->sub_func == -1)
+	{
+		delwin(wind[4]);
+		wind[4] = NULL;
+		(ts->curwin)--;
+		ts->hlight = 0;
+		ts->panel_ev = 1;
+	}
+	if(wind[4] == NULL && ts->sub_func != -1)
+	{
+		(ts->curwin)++;
+
+		switch(ts->sub_func)
+		{
+			default:	/*TEST*/
+				wind[4] = newwin(9, 50, (LINES/2)-(9/2), (COLS/2)-(50/2));
+				break;
+		}
+
+		wattron(wind[4], COLOR_PAIR(3) | A_REVERSE);
+		keypad(wind[4], TRUE);
+		ts->panel_ev = 1;
+	}
+}
+
 void handle_winch(int sig)
 {
 	for(int i = 0; i < 3; i++)
 		delwin(win[i]);
 	if(win[3])
 		delwin(win[3]);
+	if(win[4])
+		delwin(win[4]);
 	endwin();
 	refresh();
 	clear();
@@ -261,6 +302,9 @@ void mvhlight(tagst *ts)
 			break;
 		case 3: /*funcpanel*/
 			mvhlight_funcpanel(ts);
+			break;
+		case 4: /*subfuncpanel*/
+			//TODO: mvhlight_subfuncpanel(ts);
 			break;
 	}
 }
@@ -335,7 +379,15 @@ void mvhlight_funcpanel(tagst *ts)
 					ts->hlight = 0;
 				break;
 			case 10:
-				ts->choice = ts->hlight;
+				if(ts->func == 2 || ts->func == 3)
+				{
+					ts->sub_func = ts->hlight;
+					ts->hlight = 0;
+				}
+				else if(ts->func == 3)
+				{
+					/*TODO: query validation*/
+				}
 				break;
 		}
 }
@@ -347,20 +399,25 @@ void function_input(tagst *ts, int *noexit)
 		{
 			if(ts->func == -1 && i != 5)
 				ts->func = i;
-			else if(ts->func == -1 && i == 5)
+			else if(ts->func == -1 && ts->sub_func == -1 && i == 5)
 				*noexit = 0;
-			else if(ts->func != -1 && i == 5)
+			else if(ts->func != -1 && ts->sub_func == -1 && i == 5)
 				ts->func = -1;
+			else if(ts->sub_func != -1 && i == 5)
+				ts->sub_func = -1;
 			break;
 		}
 }
 
-void print_tag_screen(WINDOW *wind[4], tagst *ts)
+void print_tag_screen(WINDOW *wind[5], tagst *ts)
 {
 	print_qbar(wind[0], ts);
 	print_elem_list(wind[1], ts);
 	print_funcbar(wind[2], ts);
-	print_funcpanel(wind[3], ts);
+	if(ts->sub_func == -1)
+		print_funcpanel(wind[3], ts);
+	else
+		print_subfuncpanel(wind[4], ts);
 }
 
 void print_qbar(WINDOW *qbar_win, tagst *ts)
@@ -395,11 +452,12 @@ void print_elem_list(WINDOW *elist_win, tagst *ts)
 	int colsize = (COLS-ts->attrc+1)/ts->attrc;
 	int rest = (COLS-ts->attrc+1)%ts->attrc;
 
-	/*redraw the background if a function panel was closed*/
-	if(ts->old_curwin == 3)
+	/*redraw the background if a function panel was closed or a sub was opened*/
+	/*also redraw when the subfunc panel is closed*/
+	if(ts->panel_ev)
 	{
 		wbkgd(elist_win, COLOR_PAIR(1));
-		ts->old_curwin = -1;
+		ts->panel_ev = 0;
 	}
 
 	/*print seperators*/
@@ -429,13 +487,13 @@ void print_elem_list(WINDOW *elist_win, tagst *ts)
 			switch(i)
 			{
 				case 0: mvwprintw(elist_win, j+1, x, elem_names[j]);
-				break;
+					break;
 				case 1: mvwprintw(elist_win, j+1, x-strlen(elem_sizes[j]),
-				elem_sizes[j]);
-				break;
+													elem_sizes[j]);
+					break;
 				case 2: mvwprintw(elist_win, j+1, x-strlen(elem_tagvalues[j]),
-				elem_tagvalues[j]);
-				break;
+													elem_tagvalues[j]);
+					break;
 			}
 
 			if(ts->hlight == j && ts->curwin == 1)
@@ -516,4 +574,20 @@ void print_funcpanel(WINDOW *funcpanel, tagst *ts)
 
 	if(ts->func != -1)
 		wrefresh(funcpanel);
+}
+
+void print_subfuncpanel(WINDOW *subfunc, tagst *ts)
+{
+	for(int i = 0; i < getmaxy(subfunc); i++)
+		mvwhline(subfunc, i, 0, ' ', getmaxx(subfunc)+1);
+	box(subfunc, 0, 0);
+
+	switch(ts->sub_func)
+	{
+		default:	/*TEST*/
+			mvwprintw(subfunc, 0, 23, "TEST");
+			break;
+	}
+
+	wrefresh(subfunc);
 }
