81b0349ebb64874d32bd001d8204706eaa2dae10
taiwing <yoyo.fofo47@gmail.com>
2018-10-10 18:58:55 +0200

    181010_backup

--- 181009A-ft_printf/ft_printf.c
+++ 181009A-ft_printf/ft_printf.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/09 01:54:08 by yforeau           #+#    #+#             */
-/*   Updated: 2018/10/09 01:54:10 by yforeau          ###   ########.fr       */
+/*   Created: 2018/10/09 18:25:41 by yforeau           #+#    #+#             */
+/*   Updated: 2018/10/09 18:25:44 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-char	*ft_strstr(const char *str, const char *to_find)
+int	ft_printf(const char *format, ...)
 {
-	int			l;
-	const char	*needle;
-
-	l = ft_strlen(to_find);
-	while (*str && *to_find)
-	{
-		needle = to_find;
-		while (*str && *needle && *str == *needle)
-		{
-			str++;
-			needle++;
-		}
-		if (*needle == 0)
-			return ((char *)str - l);
-		str -= needle - to_find;
-		if (*str++ == 0)
-			return (0);
-	}
-	return (l ? 0 : (char *)str);
+	
 }
--- 181009A-ft_printf/ft_printf.h
+++ 181009A-ft_printf/ft_printf.h
@@ -1,36 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/09 01:54:08 by yforeau           #+#    #+#             */
-/*   Updated: 2018/10/09 01:54:10 by yforeau          ###   ########.fr       */
+/*   Created: 2018/10/09 18:26:09 by yforeau           #+#    #+#             */
+/*   Updated: 2018/10/09 18:28:22 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-char	*ft_strstr(const char *str, const char *to_find)
-{
-	int			l;
-	const char	*needle;
+# include <stdarg.h>
 
-	l = ft_strlen(to_find);
-	while (*str && *to_find)
-	{
-		needle = to_find;
-		while (*str && *needle && *str == *needle)
-		{
-			str++;
-			needle++;
-		}
-		if (*needle == 0)
-			return ((char *)str - l);
-		str -= needle - to_find;
-		if (*str++ == 0)
-			return (0);
-	}
-	return (l ? 0 : (char *)str);
-}
+int	ft_printf(const char *format, ...);
+
+#endif
